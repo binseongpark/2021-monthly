b2b8e1168111efa6b2b086d185214a00d423f90e
binseongpark <>
2021-01-12 22:56:06 +0900

    fix .

--- 01/vue-boilerplate/src/views/Login.vue
+++ 01/vue-boilerplate/src/views/Login.vue
@@ -1,9 +1,59 @@
 <template>
-  <div><h2>this is login</h2></div>
+  <div>
+    <!-- <b-container fluid>
+    </b-container> -->
+    <b-container fluid class="mt-4">
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+        <b-form-group
+          id="input-group-1"
+          label="Email address:"
+          label-for="input-1"
+        >
+          <b-form-input
+            id="input-1"
+            v-model="form.email"
+            type="email"
+            placeholder="Enter email"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+          <b-form-input
+            id="input-2"
+            v-model="form.name"
+            placeholder="Enter name"
+            required
+          ></b-form-input>
+        </b-form-group>
+        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+    </b-container>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      form: {
+        email: "",
+        name: "",
+        food: null,
+        checked: []
+      },
+      foods: [
+        { text: "Select One", value: null },
+        "Carrots",
+        "Beans",
+        "Tomatoes",
+        "Corn"
+      ],
+      show: true
+    };
+  }
+};
 </script>
 
 <style>
