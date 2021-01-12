a395742d2358cd073b74e79e5dbc3de824da1a3e
binseongpark <>
2021-01-12 23:52:03 +0900

    fix .

--- 01/vue-boilerplate/src/main.js
+++ 01/vue-boilerplate/src/main.js
@@ -7,6 +7,7 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import Home from '@/views/Home.vue'
 import Login from '@/views/Login.vue'
 import Main from '@/views/Main.vue'
+import NotFound from '@/views/NotFound.vue'
 
 import './app.scss'
 
@@ -22,7 +23,8 @@ Vue.use(IconsPlugin)
 const routes = [
   { path: '/', component: Home },
   { path: '/login', component: Login },
-  { path: '/main', component: Main }
+  { path: '/main', component: Main },
+  { path: '/404', component: NotFound },
 ]
 const router = new VueRouter({
   routes
