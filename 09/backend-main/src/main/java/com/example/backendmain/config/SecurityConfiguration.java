1b35b84ffb6f239f622cbcd622464bff680b0841
박성빈 <kled@kakao.com>
2021-08-19 15:38:43 +0900

    fix .

--- 09/backend-main/src/main/java/com/example/backendmain/config/SecurityConfiguration.java
+++ 09/backend-main/src/main/java/com/example/backendmain/config/SecurityConfiguration.java
@@ -1,19 +1,52 @@
 package com.example.backendmain.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private static final String[] PUBLIC = new String[]{""};
 
+    final AuthService authService;
+
+    public SecurityConfiguration(AuthService authService) {
+        this.authService = authService;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 //        super.configure(http);
         http.authorizeRequests()
-                .antMatchers("/test").permitAll();
+                .antMatchers("/test").permitAll()
+                .anyRequest().authenticated().and()
+                .cors().and().csrf().disable()
+                .headers().frameOptions().disable().and()
+                .formLogin()
+                    .loginProcessingUrl("/login/process")
+                    .usernameParameter("userId")
+                    .passwordParameter("userPw")
+//                    .loginPage("/login")
+    //                .successForwardUrl("/login/success")
+                    .failureUrl("/login")
+                    .defaultSuccessUrl("/main", true)
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .invalidateHttpSession(true)
+                .and()
+                .sessionManagement()
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(false)
+                .sessionRegistry(sessionRegistry());
     }
 
     @Override
@@ -21,5 +54,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         web.ignoring().antMatchers("/static/**", "/js/**", "/css/**", "/images/**", "/favicon.ico", "/resources/**");
     }
 
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
 
+    @Bean
+    public HttpSessionEventPublisher httpSessionEventPublisher() {
+        return new HttpSessionEventPublisher();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new DefaultPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        super.configure(auth);
+        auth.userDetailsService(authService).passwordEncoder(passwordEncoder());
+    }
 }
